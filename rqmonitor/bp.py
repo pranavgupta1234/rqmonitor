c0c255397a26cc9fa35464770df56ef4a021716d
pranavgupta1234 <pranavgupta4321@gmail.com>
2020-06-05 00:11:48 +0530

    changed http request types depending on actions and added some new views

--- rqmonitor/bp.py
+++ rqmonitor/bp.py
@@ -1,27 +1,27 @@
 from flask import (
-    Blueprint,
     current_app,
-    make_response,
     render_template,
     request,
-    send_from_directory,
-    url_for,
     jsonify
 )
 from six import string_types
 from flask import Blueprint
-from rqmonitor.utils import list_all_queues_names, list_all_possible_job_status, list_all_queues, \
-    list_jobs_in_queue_all_registries, list_jobs_on_queue, list_jobs_in_queue_registry,\
-    reformat_job_data, delete_worker, create_redis_connection, delete_queue, empty_queue
+from rqmonitor.utils import (
+                            list_all_queues_names, list_all_possible_job_status,
+                            list_all_queues, reformat_job_data, delete_workers,
+                            create_redis_connection, delete_queue, empty_queue,
+                            delete_job, cancel_job, requeue_job, get_redis_memory_used,
+                            job_count_in_queue_registry, resolve_jobs,
+                            delete_all_jobs_in_queues_registries, requeue_all_jobs_in_failed_registry,
+                            cancel_all_queued_jobs
+)
 
 from rq.connections import pop_connection, push_connection
 from rqmonitor.decorators import cache_control_no_store
 from rqmonitor.exceptions import RQMonitorException, ActionFailed
-
-from redis import Redis
-
 from rq.worker import Worker
 import logging
+
 
 logger = logging.getLogger(__name__)
 stream_handler = logging.StreamHandler()
@@ -31,6 +31,7 @@ logger.setLevel(logging.INFO)
 REDIS_RQ_HOST = 'localhost:6379'
 
 monitor_blueprint = Blueprint('rqmonitor', __name__, template_folder='templates', static_folder='static')
+
 
 # Plan to separate between HTTP and non HTTP errors by using HTTPException class
 @monitor_blueprint.errorhandler(RQMonitorException)
@@ -80,31 +81,34 @@ def home(instance_number):
     rq_queues_list = list_all_queues_names()
     rq_possible_job_status = list_all_possible_job_status()
 
-    return render_template('rqmonitor/index.html', rq_host_url= REDIS_RQ_HOST,
-                                                rq_queues_list= rq_queues_list,
-                           rq_possible_job_status= rq_possible_job_status,
-                           redis_instance_list=current_app.config.get('RQ_MONITOR_REDIS_URL'))
+    return render_template('rqmonitor/index.html',
+                            rq_host_url= REDIS_RQ_HOST,
+                            rq_queues_list= rq_queues_list,
+                            rq_possible_job_status= rq_possible_job_status,
+                            redis_instance_list=current_app.config.get('RQ_MONITOR_REDIS_URL'),
+                            redis_memory_used=get_redis_memory_used()
+                           )
 
 
 @monitor_blueprint.route('/jobs_dashboard')
 @cache_control_no_store
 def get_jobs_dashboard():
-    return render_template('rqmonitor/jobs.html', rq_host_url= REDIS_RQ_HOST)
+    return render_template('rqmonitor/jobs.html')
 
 
 @monitor_blueprint.route('/workers_dashboard')
 @cache_control_no_store
 def get_workers_dashboard():
-    return render_template('rqmonitor/workers.html', rq_host_url= REDIS_RQ_HOST)
+    return render_template('rqmonitor/workers.html')
 
 
 @monitor_blueprint.route('/queues_dashboard')
 @cache_control_no_store
 def get_queues_dashboard():
-    return render_template('rqmonitor/queues.html', rq_host_url=REDIS_RQ_HOST)
+    return render_template('rqmonitor/queues.html')
 
 
-@monitor_blueprint.route('/queues')
+@monitor_blueprint.route('/queues', methods=['GET'])
 @cache_control_no_store
 def list_queues_api():
     queue_list = list_all_queues()
@@ -124,7 +128,7 @@ def list_queues_api():
     }
 
 
-@monitor_blueprint.route('/workers')
+@monitor_blueprint.route('/workers', methods=['GET'])
 @cache_control_no_store
 def list_workers_api():
     workers_list = Worker.all()
@@ -142,21 +146,29 @@ def list_workers_api():
         )
 
     return {
-        'rq_host_url': REDIS_RQ_HOST,
         'rq_workers_count': len(rq_workers),
         'data': rq_workers,
     }
 
 
-@monitor_blueprint.route('/jobs')
+@monitor_blueprint.route('/jobs', methods=['GET'])
 @cache_control_no_store
 def list_jobs_api():
     """
-    :param request: Django GET request containing two parameters acting as filter for jobs
+    :param request: Flask GET request containing two parameters acting as filter for jobs
                     1) Jobs Status list (with these status)
                     2) queues list (to fetch queues)
     :return: rendered output
     """
+    serialised_jobs = []
+
+    start = int(request.args.get('start'))
+    length = int(request.args.get('length'))
+    draw = int(request.args.get('draw'))
+    search = request.args.get('search[value]')
+
+    request_source = request.args.get('from_datatable', None)
+
     requested_queues = request.args.getlist('queues[]')
     if requested_queues is None:
         requested_queues = list_all_queues_names()
@@ -164,100 +176,270 @@ def list_jobs_api():
     if requested_job_status is None:
         requested_job_status = list_all_possible_job_status()
 
-    job_data_for_dashboard = []
+    if not requested_queues or not requested_job_status:
+        return {
+            'data': serialised_jobs,
+        }
+
+    job_counts = []
+    total_job_count = 0
 
     for queue in requested_queues:
-        for job in list_jobs_on_queue(queue):
-            if job.get_status() in requested_job_status:
-                job_data_for_dashboard.append(reformat_job_data(job))
-        # Include jobs from registries as well
-        for job in list_jobs_in_queue_all_registries(queue):
-            if job.get_status() in requested_job_status:
-                job_data_for_dashboard.append(reformat_job_data(job))
+        for job_status in requested_job_status:
+            queue_registry_count = job_count_in_queue_registry(queue, job_status)
+            job_counts.append((queue, job_status, queue_registry_count))
+            total_job_count += queue_registry_count
 
-    request_source = request.args.get('from_datatable', None)
+    jobs = resolve_jobs(job_counts, start, length)
+
+    for job in jobs:
+        serialised_jobs.append(reformat_job_data(job))
 
     return {
-        'rq_host_url': REDIS_RQ_HOST,
-        'data': job_data_for_dashboard,
+        'draw': draw,
+        'recordsTotal': total_job_count,
+        'recordsFiltered': total_job_count,
+        'data': serialised_jobs,
     }
 
-@monitor_blueprint.route('/workers/delete')
+
+@monitor_blueprint.route('/workers/delete', methods=['POST'])
 @cache_control_no_store
-def delete_single_worker_api():
-    worker_id = request.args.get('worker_id')
-    if worker_id is None:
-        raise RQMonitorException('Worker ID not received', status_code=400)
-    try:
-        delete_worker(worker_id)
-    except ActionFailed:
-        raise RQMonitorException(f'Unable to delete {worker_id}', status_code=500)
+def delete_workers_api():
+    worker_names = []
+    if request.method == 'POST':
+        worker_id = request.form.get('worker_id', None)
+        delete_all = request.form.get('delete_all')
+        if worker_id is None and delete_all is "false":
+            raise RQMonitorException('Worker ID not received', status_code=400)
+        try:
+            if delete_all == "true":
+                for worker_instance in Worker.all():
+                    worker_names.append(worker_instance.name)
+                delete_workers(worker_names)
+            else:
+                worker_names.append(worker_id)
+                delete_workers([worker_id])
+        except ActionFailed:
+            raise RQMonitorException(f'Unable to delete worker/s', status_code=500)
 
-    return {
-        'worker_id': worker_id,
-        'message': f'Successfully deleted worker {worker_id}'
-    }
+        return {
+            'message': f'Successfully deleted worker {worker_names}'
+        }
+
+    raise RQMonitorException('Invalid HTTP Request type', status_code=400)
 
 
-@monitor_blueprint.route('/queues/delete')
+@monitor_blueprint.route('/queues/delete', methods=['POST'])
 @cache_control_no_store
 def delete_queue_api():
-    queue_id = request.args.get('queue_id', None)
-    if queue_id is None:
-        raise RQMonitorException('Queue Name not received', status_code=400)
-    try:
-        delete_queue(queue_id)
-    except ActionFailed as e:
-        raise RQMonitorException(f'Unable to delete Queue {queue_id}', status_code=500)
-    return {
-        'queue_id': queue_id,
-        'message': f'Successfully deleted {queue_id}'
-    }
+    if request.method == 'POST':
+        queue_id = request.form.get('queue_id', None)
+        if queue_id is None:
+            raise RQMonitorException('Queue Name not received', status_code=400)
+        try:
+            delete_queue(queue_id)
+        except ActionFailed as e:
+            raise RQMonitorException(f'Unable to delete Queue {queue_id}', status_code=500)
+        return {
+            'message': f'Successfully deleted {queue_id}'
+        }
+    raise RQMonitorException('Invalid HTTP Request type', status_code=400)
 
 
-@monitor_blueprint.route('/queues/empty')
+
+@monitor_blueprint.route('/queues/empty', methods=['POST'])
 @cache_control_no_store
 def empty_queue_api():
-    queue_id = request.args.get('queue_id', None)
-    if queue_id is None:
-        raise RQMonitorException('Queue Name not received', status_code=400)
-    try:
-        empty_queue(queue_id)
-    except ActionFailed as e:
-        raise RQMonitorException(f'Unable to empty Queue {queue_id}', status_code=500)
-    return {
-        'queue_id': queue_id,
-        'message': f'Successfully emptied {queue_id}'
-    }
+    if request.method == 'POST':
+        queue_id = request.form.get('queue_id', None)
+        if queue_id is None:
+            raise RQMonitorException('Queue Name not received', status_code=400)
+        try:
+            empty_queue(queue_id)
+        except ActionFailed as e:
+            raise RQMonitorException(f'Unable to empty Queue {queue_id}', status_code=500)
+        return {
+            'message': f'Successfully emptied {queue_id}'
+        }
+    raise RQMonitorException('Invalid HTTP Request type', status_code=400)
 
 
-@monitor_blueprint.route('/workers/info')
+@monitor_blueprint.route('/queues/delete/all', methods=['POST'])
+@cache_control_no_store
+def delete_all_queues_api():
+    if request.method == 'POST':
+        queue_names = [queue.name for queue in list_all_queues()]
+        for queue in list_all_queues():
+            queue.delete(delete_jobs=True)
+        return {
+            'message': f'Successfully deleted queues {queue_names}'
+        }
+    else:
+        raise RQMonitorException('Invalid HTTP Request type', status_code=400)
+
+
+@monitor_blueprint.route('/queues/empty/all', methods=['POST'])
+@cache_control_no_store
+def empty_all_queues_api():
+    if request.method == 'POST':
+        queue_names = [queue.name for queue in list_all_queues()]
+        for queue in list_all_queues():
+            queue.empty()
+        return {
+            'message': f'Successfully emptied queues {queue_names}'
+        }
+    else:
+        raise RQMonitorException('Invalid HTTP Request type', status_code=400)
+
+
+@monitor_blueprint.route('/workers/info', methods=['GET'])
 @cache_control_no_store
 def worker_info_api():
-    worker_id = None
     if request.method == 'GET':
         worker_id = request.args.get('worker_id', None)
 
-    # needs implementation to show properly on page
-    if worker_id is None:
-        return "Not found"
+        if worker_id is None:
+            raise RQMonitorException('Worker ID not received !', status_code=400)
 
-    worker_instance = Worker.find_by_key(Worker.redis_worker_namespace_prefix + worker_id)
+        worker_instance = Worker.find_by_key(Worker.redis_worker_namespace_prefix + worker_id)
+        return {
+            'worker_host_name': worker_instance.hostname.decode('utf-8'),
+            'worker_ttl': worker_instance.default_worker_ttl,
+            'worker_result_ttl': worker_instance.default_result_ttl,
+            'worker_name': worker_instance.name,
+            'worker_birth_date': worker_instance.birth_date.strftime('%d-%m-%Y %H:%M:%S')
+                                if worker_instance.birth_date is not None else "Not Available",
+            'worker_death_date': worker_instance.death_date.strftime('%d-%m-%Y %H:%M:%S')
+                                if worker_instance.death_date is not None else "Is Alive",
+            'worker_last_cleaned_at': worker_instance.last_cleaned_at.strftime('%d-%m-%Y %H:%M:%S')
+                                if worker_instance.last_cleaned_at is not None else "Not Yet Cleaned",
+            'worker_failed_job_count': worker_instance.failed_job_count,
+            'worker_successful_job_count': worker_instance.successful_job_count,
+            'worker_job_monitoring_interval': worker_instance.job_monitoring_interval,
+            'worker_last_heartbeat': worker_instance.last_heartbeat.strftime('%d-%m-%Y %H:%M:%S')
+                                if worker_instance.last_heartbeat is not None else "Not Available",
+            'worker_current_job_id': worker_instance.get_current_job_id(),
+        }
+    raise RQMonitorException('Invalid HTTP Request type', status_code=400)
+
+
+@monitor_blueprint.route('/jobs/cancel', methods=['POST'])
+@cache_control_no_store
+def cancel_job_api():
+    if request.method == 'POST':
+        job_id = request.form.get('job_id')
+        if job_id is None:
+            raise RQMonitorException('Job ID not received', status_code=400)
+        try:
+            cancel_job(job_id)
+        except ActionFailed:
+            raise RQMonitorException(f'Unable to cancel {job_id}', status_code=500)
+        return {
+            'message': f'Successfully cancelled job with ID {job_id}'
+        }
+    raise RQMonitorException('Invalid HTTP Request type', status_code=400)
+
+
+@monitor_blueprint.route('/jobs/requeue',  methods=['POST'])
+@cache_control_no_store
+def requeue_job_api():
+    if request.method == 'POST':
+        job_id = request.form.get('job_id')
+        if job_id is None:
+            raise RQMonitorException('Job ID not received', status_code=400)
+        try:
+            requeue_job(job_id)
+        except ActionFailed:
+            raise RQMonitorException(f'Unable to requeue {job_id}', status_code=500)
+
+        return {
+            'message': f'Successfully requeued job with ID {job_id}'
+        }
+    raise RQMonitorException('Invalid HTTP Request type', status_code=400)
+
+
+@monitor_blueprint.route('/jobs/delete',  methods=['POST'])
+@cache_control_no_store
+def delete_job_api():
+    if request.method == 'POST':
+        job_id = request.form.get('job_id')
+        if job_id is None:
+            raise RQMonitorException('Job ID not received', status_code=400)
+        try:
+            delete_job(job_id)
+        except ActionFailed:
+            raise RQMonitorException(f'Unable to delete {job_id}', status_code=500)
+
+        return {
+            'message': f'Successfully deleted job with ID {job_id}'
+        }
+    raise RQMonitorException('Invalid HTTP Request type', status_code=400)
+
+
+@monitor_blueprint.route('/jobs/delete/all',  methods=['POST'])
+@cache_control_no_store
+def delete_all_jobs_api():
+    if request.method == 'POST':
+        requested_queues = request.form.getlist('queues[]')
+        requested_job_status = request.form.getlist('jobstatus[]')
+        print(requested_queues, requested_job_status)
+
+        if requested_queues is None or requested_job_status is None:
+            raise RQMonitorException('No queue/status selected', status_code=400)
+
+        try:
+            delete_all_jobs_in_queues_registries(requested_queues, requested_job_status)
+        except ActionFailed:
+            raise RQMonitorException(f'Unable to delete all jobs', status_code=500)
+
+        return {
+            'message': f'Successfully deleted all jobs with'
+                       f' status as {requested_job_status} on queues {requested_queues}'
+        }
+    raise RQMonitorException('Invalid HTTP Request type', status_code=400)
+
+
+@monitor_blueprint.route('/jobs/requeue/all',  methods=['POST'])
+@cache_control_no_store
+def requeue_failed_jobs_api():
+    if request.method == 'POST':
+        requested_queues = request.form.getlist('queues[]')
+        if requested_queues is None:
+            raise RQMonitorException('No queue/s selected', status_code=400)
+        fail_count = 0
+        try:
+            fail_count = requeue_all_jobs_in_failed_registry(requested_queues)
+        except ActionFailed:
+            raise RQMonitorException(f'Unable to requeue all, remaining {fail_count} ', status_code=500)
+
+        return {
+            'message': f'Successfully requeued all jobs on queues {requested_queues}'
+        }
+    raise RQMonitorException('Invalid HTTP Request type', status_code=400)
+
+
+@monitor_blueprint.route('/jobs/cancel/all',  methods=['POST'])
+@cache_control_no_store
+def cancel_queued_jobs_api():
+    if request.method == 'POST':
+        requested_queues = request.form.getlist('queues[]')
+        if requested_queues is None:
+            raise RQMonitorException('No queue/s selected', status_code=400)
+        try:
+            fail_count = cancel_all_queued_jobs(requested_queues)
+        except ActionFailed:
+            raise RQMonitorException(f'Unable to cancel all jobs', status_code=500)
+
+        return {
+            'message': 'Successfully requeued all jobs'
+        }
+    raise RQMonitorException('Invalid HTTP Request type', status_code=400)
+
+
+@monitor_blueprint.route('/redis/memory')
+@cache_control_no_store
+def redis_memory_api():
     return {
-        'worker_host_name': worker_instance.hostname.decode('utf-8'),
-        'worker_ttl': worker_instance.default_worker_ttl,
-        'worker_result_ttl': worker_instance.default_result_ttl,
-        'worker_name': worker_instance.name,
-        'worker_birth_date': worker_instance.birth_date.strftime('%d-%m-%Y %H:%M:%S')
-                            if worker_instance.birth_date is not None else "Not Available",
-        'worker_death_date': worker_instance.death_date.strftime('%d-%m-%Y %H:%M:%S')
-                            if worker_instance.death_date is not None else "Is Alive",
-        'worker_last_cleaned_at': worker_instance.last_cleaned_at.strftime('%d-%m-%Y %H:%M:%S')
-                            if worker_instance.last_cleaned_at is not None else "Not Yet Cleaned",
-        'worker_failed_job_count': worker_instance.failed_job_count,
-        'worker_successful_job_count': worker_instance.successful_job_count,
-        'worker_job_monitoring_interval': worker_instance.job_monitoring_interval,
-        'worker_last_heartbeat': worker_instance.last_heartbeat.strftime('%d-%m-%Y %H:%M:%S')
-                            if worker_instance.last_heartbeat is not None else "Not Available",
-        'worker_current_job_id': worker_instance.get_current_job_id(),
+        'redis_memory_used': get_redis_memory_used()
     }
