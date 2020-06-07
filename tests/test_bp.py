e1137be68a4958efe575554a4a0b733709b686d2
pranavgupta1234 <pranavgupta4321@gmail.com>
2020-06-08 00:49:43 +0530

    added tests

---
+++ tests/test_bp.py
@@ -0,0 +1,40 @@
+import unittest
+import sys
+import os
+import redis
+from rq import  pop_connection, push_connection
+
+sys.path.insert(0, os.path.join(os.getcwd(), "../"))
+
+from rqmonitor.cli import create_app_with_blueprint
+
+HTTP_OK = 200
+HTTP_BAD_REQUEST = 400
+HTTP_INTERNAL_ERROR = 500
+
+class TestDecorators(unittest.TestCase):
+    redis_client = None
+
+    def get_redis_client(self):
+        if self.redis_client is None:
+            self.redis_client = redis.Redis()
+        return self.redis_client
+
+    def setUp(self):
+        self.app = create_app_with_blueprint()
+        self.app.testing = True
+        self.app.config['RQ_MONITOR_REDIS_URL'] = 'redis://127.0.0.1'
+        self.app.redis_conn = self.get_redis_client()
+        push_connection(self.get_redis_client())
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        pop_connection()
+
+    def test_dashboard_ok(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, HTTP_OK)
+
+    def test_redis_index_before_request(self):
+        response = self.client.get('/', query_string={ 'redis_instance_index' : 100})
+        self.assertEqual(response.status_code, HTTP_BAD_REQUEST)
---
+++ tests/test_decorator.py
@@ -0,0 +1,35 @@
+import unittest
+import sys
+import os
+import redis
+from rq import  pop_connection, push_connection
+
+sys.path.insert(0, os.path.join(os.getcwd(), "../"))
+
+from rqmonitor.cli import create_app_with_blueprint
+
+HTTP_OK = 200
+
+class TestDecorators(unittest.TestCase):
+    redis_client = None
+
+    def get_redis_client(self):
+        if self.redis_client is None:
+            self.redis_client = redis.Redis()
+        return self.redis_client
+
+    def setUp(self):
+        self.app = create_app_with_blueprint()
+        self.app.testing = True
+        self.app.config['RQ_MONITOR_REDIS_URL'] = 'redis://127.0.0.1'
+        self.app.redis_conn = self.get_redis_client()
+        push_connection(self.get_redis_client())
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        pop_connection()
+
+    def test_cache_control_no_store(self):
+        response = self.client.get('/')
+        self.assertEqual(response.headers['Cache-Control'], 'no-store')
+
