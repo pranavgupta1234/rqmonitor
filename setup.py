3369e5f39a74c9fb73e083f829ff1186a633329a
pranavgupta1234 <pranavgupta4321@gmail.com>
2020-06-08 21:19:28 +0530

    fixed syntax error in setup.py

--- setup.py
+++ setup.py
@@ -30,9 +30,9 @@ setuptools.setup(
 
         'License :: OSI Approved :: Apache Software License',
 
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
     ],
  )
