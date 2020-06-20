99ac01cbb348a0b8b3b1c038d6d35e85b3913c5f
pranavgupta1234 <pranavgupta4321@gmail.com>
2020-06-20 11:50:21 +0530

    added previous exception traceback in RQMonitorException

--- rqmonitor/exceptions.py
+++ rqmonitor/exceptions.py
@@ -1,10 +1,13 @@
-from flask import jsonify
-from werkzeug.exceptions import HTTPException
+import traceback
+
 
 class RQMonitorException(Exception):
-    status_code = 400
-
-    def __init__(self, message, status_code=None, payload=None):
+    '''
+    used to catch any possible exception that might occur in used libraries
+    or while performing some action and display gracefully inside rqmonitor
+    '''
+    def __init__(self, message='RQ Monitor Global Exception',
+                 status_code=500, payload=None):
         Exception.__init__(self)
         self.message = message
         if status_code is not None:
@@ -14,8 +17,5 @@ class RQMonitorException(Exception):
     def to_dict(self):
         rv = dict(self.payload or ())
         rv['message'] = self.message
+        rv['traceback'] = ''.join(traceback.format_tb(self.__traceback__))
         return rv
-
-
-class ActionFailed(Exception):
-    pass
